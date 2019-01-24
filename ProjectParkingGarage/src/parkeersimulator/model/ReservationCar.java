9a579ef0d45488fea8b26209beba4eca1f64ee65
Alice <alice@bkkpkht.gq>
2019-01-24 15:06:32 +0100

    Resolved merge conflict by incorporating both suggestions.

---
+++ ProjectParkingGarage/src/parkeersimulator/model/ReservationCar.java
@@ -0,0 +1,41 @@
+package parkeersimulator.model.car;
+
+import java.util.Random;
+
+import parkeersimulator.model.ParkingGarageModel.CarType;
+
+import java.awt.*;
+
+/**
+ * The model of a car without a subscription.
+ */
+public class ReservationCar extends Car {
+	///The color this type of car should have.
+	private static final Color COLOR=Color.yellow;
+	
+	/**
+	 * The constructor for ReservationCar
+	 */
+    public ReservationCar() {
+    	Random random = new Random();
+    	int stayMinutes = (int) (15 + random.nextFloat() * 3 * 60);
+        this.setMinutesLeft(stayMinutes);
+    }
+    
+    /**
+     * @return the color of the car used in the simulation.
+     */
+    public Color getColor(){
+    	return COLOR;
+    }
+    
+	@Override
+	public CarType getCarType() {
+		return CarType.RESERVERATION_CAR;
+	}
+
+	@Override
+	public boolean getHasToPay() {
+		return true;
+	}
+}
